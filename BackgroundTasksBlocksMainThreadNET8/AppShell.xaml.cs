1f7dc2451645085e210e901225ea5abef71bbde2
Mathias Jørgensen <mathias.flintrup.jorgensen@gmail.com>
2025-09-09 15:28:53 +0200

    Add files via upload

---
+++ BackgroundTasksBlocksMainThreadNET8/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BackgroundTasksBlocksMainThreadNET8
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+		}
+	}
+}
